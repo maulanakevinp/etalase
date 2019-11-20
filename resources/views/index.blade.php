0ffc75c4d582a4ebcc7d1ac683758ba1c3170d5d
Maulana Kevin Pradana <44237629+maulanakevinpradana@users.noreply.github.com>
2019-11-20 09:34:35 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -71,6 +71,7 @@
                 </div>
             @endforeach
         </div>
+        {{ $images->links() }}
     </div>
     
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
