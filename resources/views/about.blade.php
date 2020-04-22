e50360c5090656649a8a6587bd1f534d2e07c30f
Bob <zohalalfinas@gmail.com>
2020-04-23 06:09:34 +0700

    menambahkan about controller

---
+++ resources/views/about.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>UKMK Etalase</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="{{ asset('logo/logo etalase.png') }}" type="image/x-icon">
+
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet"
+        href="{{ url('/') }}/assets/snapshot/{{ url('/') }}/assets/snapshot/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/animate.css">
+
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/aos.css">
+
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/jquery.timepicker.css">
+
+
+    <!-- <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/flaticon.css"> -->
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/flaticonBidang.css">
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/icomoon.css">
+    <link rel="stylesheet" href="{{ url('/') }}/assets/snapshot/css/style.css">
+</head>
+
+<body>
+
+
+<<section class="ftco-about img ftco-section" id="about">
+        <div class="container">
+            <div class="row d-flex no-gutters">
+                <div class="col-md-6 col-lg-6 d-flex">
+                    <div class="img-about img d-flex align-items-stretch">
+                        <div class="overlay"></div>
+                        <div class="img img-video d-flex align-self-stretch align-items-center"
+                            style="background-image:url({{ url('/') }}/assets/snapshot/images/about-2.jpg);">
+                            <div class="video justify-content-center">
+                                <a href="https://vimeo.com/45830194"
+                                    class="icon-video popup-vimeo d-flex justify-content-center align-items-center">
+                                    <span class="ion-ios-play"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-6 pl-md-5">
+                    <div class="heading-section ftco-animate">
+                        <h2 class="mb-4">Etalase... <br></h2>
+                        <p>Etalase adalah lkajeqjwdnm lasnlqw smd clqwkndna slkjdq lkjsae lkajseqj lkjqkjwe alkjsd
+                            alksjdq
+                            alksjdljqw lkdjqlkjd alskj dlqkj lkjaslkdj ljelk lka d qlkwjldjq kljsf ioeurlns ch kjw k
+                            jhkqwh kajdh
+                            klndqj lkj asdnmnqw kjasd lkweoiu kjasd khwe</p>
+                        <div class="counter-wrap ftco-animate d-flex my-md-4">
+                            <div class="text">
+                                <p class="mb-4">
+                                    <span class="number" data-number="120">0</span>
+                                    <span>Project complete</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="d-flex w-100">
+                            <div class="img img-about-2 mr-2"
+                                style="background-image:url({{ url('/') }}/assets/snapshot/images/about.jpg);"></div>
+                            <div class="img img-about-2 ml-2"
+                                style="background-image:url({{ url('/') }}/assets/snapshot/images/about-3.jpg);"></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="col-md-3" style="padding: 1.5rem">
+                                    <a href="#">
+                                        <p class="btn-kotak">Profil Etalase <span
+                                                class="ion-ios-arrow-round-forward"></span></p>
+                                    </a>
+                                </div>
+                                <div class="col-md-3" style="padding: 1.5rem">
+                                    <a href="{{url('/about')}}">
+                                        <p class="btn-kotak">Sejarah Etalase <span
+                                                class="ion-ios-arrow-round-forward"></span></p>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- <blockquote class="blockquote mt-5">
+		          	<p class="mb-2">"Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.."</p>
+		          	<span>&mdash; Lucy Lee</span>
+		          </blockquote> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+<script src="{{ url('/') }}/assets/snapshot/js/jquery.min.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/popper.min.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/bootstrap.min.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/jquery.easing.1.3.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/jquery.waypoints.min.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/jquery.stellar.min.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/owl.carousel.min.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/jquery.magnific-popup.min.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/aos.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/jquery.animateNumber.min.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/scrollax.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
+    </script>
+    <script src="{{ url('/') }}/assets/snapshot/js/google-map.js"></script>
+    <script src="{{ url('/') }}/assets/snapshot/js/main.js"></script>
+</body>
+
+</html>
